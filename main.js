17141d442af29a7c4626b0cc1152ffbdf59efb1d
draeriel <danieljuanbarbosa@gmail.com>
2019-03-10 02:39:11 +0100

    Añadir lista de la compra a localStorage y mostrar lista

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 shopsList = [];
+shoppingList = [];
 
 window.onload = function () {
     this.setShopsList();
+    if (this.shopsList.length > 0) {
     this.updateShopsList();
+    this.updateShoppingList();
+    }
+    console.log(this.shoppingList)
+
 }
 
 function setShopsList() {
@@ -14,28 +20,66 @@ function setShopsList() {
     }
 }
 
-function updateShopsList() {
-    if (this.shopsList.length > 0) {
+function updateShopsList() {   
         let div = document.createElement('div');
         let shopsSelect = document.createElement('select');
+        shopsSelect.setAttribute('id', 'shopsSelect');
         Object.keys(this.shopsList).forEach(shop => {
             let shopsOption = document.createElement('option');
             shopsOption.innerHTML = this.shopsList[shop].name;
             shopsSelect.appendChild(shopsOption);
         });
+        let input = document.createElement('input');
+        input.setAttribute('id', 'shoppingListForm');
+        let button = document.createElement('button');
+        button.innerHTML = "Añadit a la lista de la compra";
+        button.setAttribute('onclick', 'addToShoppingList()');
         div.innerHTML = "Seleccione una tienda: ";
         div.appendChild(shopsSelect);
+        div.appendChild(input);
+        div.appendChild(button);
+
         document.body.insertBefore(div, document.getElementById('shopForm'));
-    }
 }
 
 function addShop() {
     let shopName = document.getElementById('shopName').value;
     this.shopsList.push({
         name: shopName,
-        shoppingList: {}
+        shoppingList: []
     });
     localStorage.setItem('shops', JSON.stringify(this.shopsList));
-    this.updateShopsList();
 }
+
+function updateShoppingList() {
+    Object.keys(this.shopsList).forEach( shop => {
+        console.log( this.shopsList[shop].name)
+        if (this.shopsList[shop].name === document.getElementById('shopsSelect').value) {
+            this.displayShoppingList(this.shopsList[shop].shoppingList);
+        };
+    });
+}
+
+function displayShoppingList(shoppingList) {
+    let div = document.createElement('div');
+        let ul = document.createElement('ul');
+    shoppingList.forEach(element => {
+        let li = document.createElement('li');
+        li.innerHTML = element;
+        ul.appendChild(li);
+    });
+    div.appendChild(ul);
+    document.body.appendChild(div);
+}
+
+function addToShoppingList() {
+    Object.keys(this.shopsList).map( shop => {
+        if (this.shopsList[shop].name === document.getElementById('shopsSelect').value) {
+            shoppingList = this.shopsList[shop].shoppingList;
+            this.shopsList[shop].shoppingList.push(document.getElementById('shoppingListForm').value);           
+        };
+    });
+    localStorage.setItem('shops', JSON.stringify(this.shopsList));
+}
+
 
